e858fd313efa215390df311d72263d686506d3f1
AbN13 <abnerleandro96@gmail.com>
2022-05-25 22:43:53 -0300

    eventos

---
+++ src/index.js
@@ -0,0 +1,29 @@
+console.log("Informe a data do evento:");
+let dataDoEvento = 15;
+console.log("Dia" , dataDoEvento);
+
+if (dataDoEvento <= 18) {
+    console.log("Evento Permitido");
+} else {
+    console.log("Data inválida, Por favor insira outra data");
+};
+
+console.log("Informe sua idade:");
+let idade = 19;
+console.log(idade , "anos");
+
+if (idade >= 18) {
+    console.log("Idade permitida");
+} else {
+    console.log("Cadastro não permitido, idade inválida");
+};
+
+console.log("Listar participantes e palestrantes:");
+let participantes = 99;
+console.log(participantes , "participantes");
+
+if (participantes <= 100) {
+    console.log("Evento cadastrado com sucesso");
+} else {
+    console.log("Cadastro não permitido, limite de participantes excedido");
+};
